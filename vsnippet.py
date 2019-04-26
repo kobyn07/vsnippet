b7bed53b1c814c5249b09065f2624954680d849c
kobyn <mail@kobyn.net>
2019-04-26 22:41:38 +0900

    最低限の実装

---
+++ vsnippet.py
@@ -0,0 +1,85 @@
+# -*- cording: utf-8 -*-
+"""
+Visual Studio Code のコードスニペットを簡単に作成するツール
+"""
+
+import tkinter as tk
+import pyperclip
+
+
+class Application(tk.Frame):
+
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.master.title("vsnippet")
+        self.master.geometry("1075x500")
+
+        self.prefix = ""
+        self.description = ""
+        self.body = ""
+
+        # prefix の入力
+        prefix_ttl = tk.Label(self, text="prefix")
+        prefix_ttl.grid(row=0, column=0, sticky=tk.E)
+        self.prefix_ent = tk.Entry(self, width=40)
+        self.prefix_ent.grid(row=0, column=1, padx=10, pady=10)
+
+        # description の入力
+        desc_ttl = tk.Label(self, text="description")
+        desc_ttl.grid(row=1, column=0, sticky=tk.E)
+        self.desc_ent = tk.Entry(self, width=40)
+        self.desc_ent.grid(row=1, column=1, padx=10, pady=10)
+
+        # body の入力
+        body_ttl = tk.Label(self, text="body")
+        body_ttl.grid(row=2, column=0, padx=10, pady=10)
+        self.body_txt = tk.Text(self, width=50, wrap=tk.NONE)
+        self.body_txt.grid(row=3, column=0, padx=15)
+
+        # body の整形出力
+        covted_ttl = tk.Label(self, text="出力")
+        covted_ttl.grid(row=2, column=2, padx=30, pady=10)
+        self.covted_txt = tk.Text(self, width=50, wrap=tk.NONE)
+        self.covted_txt.grid(row=3, column=2, padx=15)
+
+        # 変換ボタン
+        covt_btn = tk.Button(self, text="変換", padx=20, pady=20,
+                             font=(10), command=self.clicked_on_convert_Button)
+        covt_btn.grid(row=3, column=1)
+
+        # クリップボードにコピーするかどうかのチェックボックス
+        self.clip_bool = tk.BooleanVar()
+        self.clip_bool.set(True)
+        self.clip_btn = tk.Checkbutton(
+            self, variable=self.clip_bool, text="クリップボードにコピーする")
+        self.clip_btn.grid(row=4, sticky=tk.W)
+
+        # 消去ボタン
+        self.clr_btn = tk.Button(
+            self, text="消去", command=self.clicked_on_clear_button)
+        self.clr_btn.grid(row=4, column=1)
+
+    def clicked_on_convert_Button(self):
+        "変換ボタンを押したときの挙動"
+        self.init_txt(self.covted_txt)
+
+    def clicked_on_clear_button(self):
+        "消去ボタンを押したときの挙動"
+        self.init_txt(self.body_txt)
+        self.init_txt(self.covted_txt)
+        self.init_ent(self.desc_ent)
+        self.init_ent(self.prefix_ent)
+
+    def init_txt(self, txt):
+        "テキストボックスの文字列を消去する"
+        txt.delete("1.0", "end")
+
+    def init_ent(self, ent):
+        "エントリーボックスの文字列を消去する"
+        ent.delete("0", "end")
+
+
+if __name__ == "__main__":
+    f = Application()
+    f.pack()
+    f.mainloop()
